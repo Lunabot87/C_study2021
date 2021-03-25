80c4ae08a167e83c45bf01d4f1c21f7c4f0f75c4
Lunabot87 <rkwoskek@naver.com>
2021-03-25 21:40:47 +0900

    C++ 1.1강

---
+++ Chapter1/C_1.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //i(nput)o(utput)stream 
+                    //전처리기(preprocessor directive)
+
+int main(void){
+    (1 + 2) * (3 + 4); //expression(수식)
+
+    int x = 2;  //정수형(int) 변수(x)를 선언
+                //정수형데이터를 담아둘수 있는 메모리를 할당
+                //리터럴(literal)
+
+    int y = x + 3;
+
+    std::cout << y;
+
+    return 0;
+}
